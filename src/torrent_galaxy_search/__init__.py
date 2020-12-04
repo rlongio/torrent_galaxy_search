264f3c791800d86d253d4ecf9f8e508a53d0132e
Ryan Long <ryan@ryanlong.tech>
2020-12-04 10:23:40 -0500

    Moved test code out of __init__ and into docs

--- src/torrent_galaxy_search/__init__.py
+++ src/torrent_galaxy_search/__init__.py
@@ -1,15 +1,1 @@
-from typing import List
-from torrent_galaxy_search.search import Search, Result
 
-
-def main():
-    results: List[Result] = Search.by_IMDB_ID("tt0095016")
-
-    for r in results:
-        print("----------------------")
-        print(r.views)
-        print(r.magnet_url)
-
-
-if __name__ == "__main__":
-    main()
